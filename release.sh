a03990b0a422324ac2b4823931b93d3beec50153
Michał Zieliński <michal@zielinscy.org.pl>
2018-12-30 10:16:32 +0100

    add missing esptool.exe

--- release.sh
+++ release.sh
@@ -2,7 +2,8 @@
 set -e
 rm -r release || true
 rm arduino-husarnet-esp32-v1.zip || true
-mkdir release
+mkdir -p release/tools
+(cd release/tools; unzip ../../esptool-2.3.1-windows.zip; mv esptool/esptool.exe .; rmdir esptool)
 names="$(git ls-files) $(find ./libraries/BLE/*)"
 for name in $names; do
     mkdir -p release/$(dirname $name)
